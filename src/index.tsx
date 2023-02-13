1532f2b5b08f29f62f391dbfa19a745ed51c8672
adammertel <mertel.adam@gmail.com>
2023-02-13 18:44:26 +0100

    Restructure folders

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import App from "./containers/app";
 import { MantineProvider } from "@mantine/core";
 
 const root = ReactDOM.createRoot(
